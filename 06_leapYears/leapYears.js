5cbf8339a414b45a67a82da74edb90d99a120bac
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-10-29 18:23:55 +0200

    Edit exercise with theOdingProject's solution.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+/*
+    // Leap years are divisible by four
+    if ( year % 4 === 0 ){
+        // Years divisible by 100 are not leap years.
+        // Unless they are divisible by 400.
+        if ( year % 100 === 0 ){
+            return year % 400 === 0;
+        }        
+        return true;
+    }
+    return false;
+*/
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 == 0 );
 };
 
 // Do not edit below this line
