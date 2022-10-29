c507f1a96dc30bbce112ca43feeb5a5852cc5ad1
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-10-29 17:50:07 +0200

    End 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if ( start < 0 || end < 0 ||
+         typeof(start) !== 'number' || typeof(end) !== 'number')
+        return 'ERROR';
+    
+    if (start > end){
+        let temp = start;
+        start =  end;
+        end = temp;
+    }
+    sum = start;
+    for (let i = start+1; i <= end;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
