6648ad89b8f834fac2f027c9970073e8d4f17ca1
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-10-29 18:39:12 +0200

    End 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const ftoc = function() {
+const ftoc = function(f) {
+    // Fahrenheit to celsius;
+    let c = (f - 32) * 5/9;
 
+    // + sign means convert to number
+    return +c.toFixed(1);
 };
 
-const ctof = function() {
+const ctof = function(c) {
+    // Celsius to Fahrenheit
+    let f = c * 9/5 + 32;
+
+    // + sign means convert to number    
+    return +f.toFixed(1);
 
 };
 
