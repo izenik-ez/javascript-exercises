4bb54d34d933068b85fc5b560e1e4de8c8556d1b
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-10-29 14:18:47 +0200

    End 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let result = '';
 
+    if ( times < 0 ){
+        result =  'ERROR';
+    } else if ( string == '' ){
+        result = '';
+    } else {
+        for(let i = 0 ; i < times ; i++){
+            result = result + string;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
