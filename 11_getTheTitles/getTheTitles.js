9770408761fd2921f9cd4c9c76542e896dc7b146
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-11-02 19:08:02 +0100

    End getTheTitle exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titleArray = [];
+    // for(const book in array){
+    //     console.log(book);
+    //     titleArray.push(array[book].title);
+    // }
+    array.forEach((item) => {
+        titleArray.push(item.title);
+    });
+    return titleArray;
 };
 
 // Do not edit below this line
