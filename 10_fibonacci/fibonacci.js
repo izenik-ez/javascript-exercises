e089ebeb19ddd5564dc412b482c1483360cccce7
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-11-02 18:54:38 +0100

    End fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    let iterations = parseInt(nth);
+    let n1 = 1;
+    let n2 = 1;
+    let result = 0;
+    
+    if (nth < 0) return "OOPS";
+    if (nth <= 2) return 1;
+    
+    for(let i = 0; i < iterations-2 ; ++i){
+        result = n1 + n2;
+        n1 = n2;
+        n2 = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
