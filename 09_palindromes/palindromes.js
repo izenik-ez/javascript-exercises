f4837dffc655e18626a6a676edbdb0da5963e20f
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-11-02 18:30:13 +0100

    End palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = str.replace(/[^\w]/g,"").toLowerCase();
+    return newStr ===  newStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
