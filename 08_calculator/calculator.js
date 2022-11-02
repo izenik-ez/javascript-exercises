a06e5e876c732847ce71f0f40adb3a87b7b27c54
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-11-02 18:14:20 +0100

    End Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(n1,n2) {
+    return n1+n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2) {
+    return n1-n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    return array.reduce((total, i) => { return total + i}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.reduce((total, i) => { return total * i}, 1);
 };
 
-const power = function() {
-	
+const power = function(base, expt) {
+    return base**expt;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+    let fact = n;
+    if(n <= 1){
+        return 1;
+    }else{
+        fact *= factorial(n-1);
+    }
+    return fact;
 };
 
 // Do not edit below this line
