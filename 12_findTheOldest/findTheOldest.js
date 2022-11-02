64058b22868d5756d7e82c297aecebe572e3bff3
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-11-02 19:55:42 +0100

    End findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const getAge = function (p){
+    if(!p.yearOfDeath){
+        p.yearOfDeath = new Date().getFullYear();
+    }
+    
+    return p.yearOfDeath - p.yearOfBirth;
+}
 
-};
+
+const findTheOldest = function(array) {
+    return array.reduce((old, person) => {
+        return getAge(old) < getAge(person) ? person : old;
+    });
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
